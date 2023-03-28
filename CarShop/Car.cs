fd2a357323bd2cb6f8173a4f1d20d747ee61d20c
Sergio Luis Francisco Pinheiro <spinheiro.lf@gmail.com>
2023-03-28 10:46:21 -0300

    Adicionar arquivos de projeto.

---
+++ CarShop/Car.cs
@@ -0,0 +1,24 @@
+﻿namespace CarShop
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string NumeroDoChasse { get; set; }
+
+        public string NumeroDoMotor { get; set; }
+
+        public double CustoDeProducao { get; set; }
+
+        public double Imposto { get; set; }
+
+        public double PrecoDeVenda
+        {
+            get
+            {
+                double custo = CustoDeProducao + Imposto;
+                double valorDeVenda = (custo * 100) / 75;
+                return valorDeVenda;
+            }
+        }
+    }
+    }
---
+++ CarShop/Flex.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace CarShop
+{
+public class Flex : Car
+{
+    public int NumeroDePortas { get; set; }
+    public int Cilindradas { get; set; }
+
+    public Flex(double custoDeProducao)
+    {
+        
+        Imposto = ((20 / 100) * custoDeProducao);
+      
+    }
+    } 
+}
